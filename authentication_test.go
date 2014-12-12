6a4428af325102ced7ee79f38af65661e0d52d3f
Don Petersen <don@donpetersen.net>
2014-12-12 17:29:59 +0000

    Move some shared test code into a place.
    Which may or may not be appropriate.

--- authentication_test.go
+++ authentication_test.go
@@ -1,43 +1,6 @@
 package clcgo
 
 import "testing"
-import "fmt"
-
-type getHandlerCallback func(string, string) (string, error)
-type handlerCallback func(string, authParameters) (string, error)
-
-//TODO NEED TO MOVE THIS SOMEWHERES
-type testRequestor struct {
-	GetHandlers map[string]getHandlerCallback
-	Handlers    map[string]handlerCallback
-}
-
-func newTestRequestor() testRequestor {
-	return testRequestor{
-		Handlers:    make(map[string]handlerCallback),
-		GetHandlers: make(map[string]getHandlerCallback),
-	}
-}
-
-// TODO: And a count for verification
-func (r *testRequestor) registerHandler(url string, callback handlerCallback) {
-	r.Handlers[url] = callback
-}
-
-func (r *testRequestor) registerGetHandler(url string, callback getHandlerCallback) {
-	r.GetHandlers[url] = callback
-}
-
-func (r testRequestor) PostJSON(url string, v interface{}) ([]byte, error) {
-	callback, found := r.Handlers[url]
-	if found {
-		// TODO error checking on coercion and make this more flexible
-		response, err := callback(url, v.(authParameters))
-		return []byte(response), err
-	} else {
-		return nil, fmt.Errorf("There is no handler for the URL '%s'", url)
-	}
-}
 
 func TestFetchCredentialsWithGoodCredentials(t *testing.T) {
 	r := newTestRequestor()
---
+++ helpers_test.go
@@ -0,0 +1,38 @@
+package clcgo
+
+import "fmt"
+
+type getHandlerCallback func(string, string) (string, error)
+type handlerCallback func(string, authParameters) (string, error)
+
+type testRequestor struct {
+	GetHandlers map[string]getHandlerCallback
+	Handlers    map[string]handlerCallback
+}
+
+func newTestRequestor() testRequestor {
+	return testRequestor{
+		Handlers:    make(map[string]handlerCallback),
+		GetHandlers: make(map[string]getHandlerCallback),
+	}
+}
+
+// TODO: And a count for verification
+func (r *testRequestor) registerHandler(url string, callback handlerCallback) {
+	r.Handlers[url] = callback
+}
+
+func (r *testRequestor) registerGetHandler(url string, callback getHandlerCallback) {
+	r.GetHandlers[url] = callback
+}
+
+func (r testRequestor) PostJSON(url string, v interface{}) ([]byte, error) {
+	callback, found := r.Handlers[url]
+	if found {
+		// TODO error checking on coercion and make this more flexible
+		response, err := callback(url, v.(authParameters))
+		return []byte(response), err
+	}
+
+	return nil, fmt.Errorf("There is no handler for the URL '%s'", url)
+}
